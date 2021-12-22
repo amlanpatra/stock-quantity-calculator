af6be2886be987bba546af8abf79c37cdfff45fa
Amlan Patra <mark.maze0r@icloud.com>
2021-12-22 09:01:00 +0530

    calculations modified in calc_futures.py

--- calc_futures.py
+++ calc_futures.py
@@ -3,24 +3,24 @@ def FuturesBrokerageCalc(price, sl, quantity=50):
     sell = price
     turnover = (buy+sell)*quantity
     brokerage = 40
-    stt = round(((sell*quantity)*(0.025))/100, 2)
-    exchange_txn_charge = round((turnover*0.00345)/100, 2)
+    stt = round(((sell*quantity)*(0.01))/100, 2)
+    exchange_txn_charge = round((turnover*0.002)/100, 2)
     gst = round((brokerage+exchange_txn_charge)*0.18, 2)
     sebi = round((10/10000000)*turnover, 2)
-    stamp = round((0.003*(buy*quantity)/100), 2) if (buy *
-                                                     quantity) < 10000000 else 300
+    stamp = round((0.002*(buy*quantity)/100), 2) if (buy *
+                                                     quantity) < 10000000 else 200
 
     total_brokerages = round(brokerage +
                              stt+exchange_txn_charge+gst+sebi+stamp, 2)
 
     breakeven = round((abs(total_brokerages))/quantity, 2)
-    risk = total_brokerages + (sl * quantity)
+    risk = round(total_brokerages + (sl * quantity), 2)
 
     arr = [total_brokerages, breakeven, risk]
     return arr
 
 
-# arr = FuturesBrokerageCalc(37500, 23)
+# arr = FuturesBrokerageCalc(17000, 10)
 # print("Risk : {}".format(str(arr[2])))
 # print("breakeven : {}".format(str(arr[1])))
 # print("Brokerage is : {}".format(str(arr[0])))
